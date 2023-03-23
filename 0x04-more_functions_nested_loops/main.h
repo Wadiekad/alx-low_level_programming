461a73964932f3acb74b639b4e6e18cb2ea92f0a
wadiekad <user.name>
2023-03-23 15:01:27 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,15 +1,16 @@
-#ifndef MAIN_H
-#define MAIN_H
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
-#endif
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase character
+ * @c: input
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+
+	else
+	       return (0);
+}
